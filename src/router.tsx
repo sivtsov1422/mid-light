3e5be217da96902f790e9dd3a02666c01400bdac
Ivan <komov_03@list.ru>
2025-02-11 15:31:28 +0300

    routerChange

--- src/router.tsx
+++ src/router.tsx
@@ -5,7 +5,7 @@ import Layout from "./components/Layout/Layout";
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "mid-light",
     element: <Layout />,
     children: [
       { index: true, element: <Home /> },
